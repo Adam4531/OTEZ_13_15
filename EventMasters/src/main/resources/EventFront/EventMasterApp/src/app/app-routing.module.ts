a8fee2d647fa19260db8629c3d8096ee6fbbaf3f
Cichyy22 <ok.kuba@wp.pl>
2023-01-12 16:27:15 +0100

    technical_fault form and subsite start

--- EventMasters/src/main/resources/EventFront/EventMasterApp/src/app/app-routing.module.ts
+++ EventMasters/src/main/resources/EventFront/EventMasterApp/src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { CompanyComponent } from './company/company.component';
+import { EventsComponent } from './events/events.component';
+import { OrganizeComponent } from './organize/organize.component';
+import { SellComponent } from './sell/sell.component';
+import { TechnicalFaultComponent } from './technical-fault/technical-fault.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'report', component: TechnicalFaultComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'organize', component: OrganizeComponent },
+  { path: 'sell', component: SellComponent },
+  { path: 'company', component: CompanyComponent },
 
 ];
 
--- EventMasters/src/main/resources/EventFront/EventMasterApp/src/app/app.module.ts
+++ EventMasters/src/main/resources/EventFront/EventMasterApp/src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { TechnicalFaultComponent } from './technical-fault/technical-fault.component';
+import { CartComponent } from './cart/cart.component';
+import { EventsComponent } from './events/events.component';
+import { OrganizeComponent } from './organize/organize.component';
+import { SellComponent } from './sell/sell.component';
+import { CompanyComponent } from './company/company.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective
+    DropdownDirective,
+    TechnicalFaultComponent,
+    CartComponent,
+    EventsComponent,
+    OrganizeComponent,
+    SellComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

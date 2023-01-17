d22ff470c9ed22221c1b62ab504ad3e66a2abfd3
Adam4531 <70113220+Adam4531@users.noreply.github.com>
2023-01-17 17:59:04 +0100

    Payment validators plugged in

--- EventMasters/src/main/java/pl/otez/payment/validation/PaymentDate.java
+++ EventMasters/src/main/java/pl/otez/payment/validation/PaymentDate.java
@@ -1,7 +1,15 @@
 package pl.otez.payment.validation;
 
+import pl.otez.interfaces.Validator;
+
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.time.LocalDate;
 
 @Embeddable
-public class PaymentDate {
+public class PaymentDate implements Validator {
+
+    @Column
+    private LocalDate date;
+
 }
--- EventMasters/src/main/java/pl/otez/payment/validation/PaymentPrice.java
+++ EventMasters/src/main/java/pl/otez/payment/validation/PaymentPrice.java
@@ -1,7 +1,16 @@
 package pl.otez.payment.validation;
 
+import pl.otez.interfaces.Validator;
+
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.math.BigDecimal;
 
 @Embeddable
-public class PaymentPrice {
+public class PaymentPrice implements Validator {
+
+    @Column
+    private BigDecimal price;
+
+
 }
